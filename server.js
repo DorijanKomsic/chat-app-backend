d214393aaa10b6b65ede991a4170f75e26111a26
DorijanKomsic <komsic.dorijan@gmail.com>
2022-12-18 19:12:25 +0100

    Created link to mongodb

--- server.js
+++ server.js
@@ -3,16 +3,19 @@ const app = express();
 
 const cors = require('cors');
 
+//Server ovdje parsa sve POST/PUT requestove u application/json ili application/x-www-form-urlencoded 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(cors());
 
+require('./connection');
+
+//Ovdje kreiramo i vezemo socket.io i node.js http server jedno s drugim
 const server = require('http').createServer(app)
 const port = 5001;
 const io = require('socket.io')(server, {
     cors: {
         origin: 'http://localhost:3000',
-        methods: ['GET','POST']
     }
 })
 
